4599b08007e21dad27748d15e34a5bf24e63dbbd
Bernd Schoolmann <quextenmail@googlemail.com>
2018-03-30 18:13:46 +0200

    Change email login message

--- src/components/LoginEmail.vue
+++ src/components/LoginEmail.vue
@@ -1,7 +1,7 @@
 <template>
 <v-content>
   <div style="text-align:center; padding-top:20px;">
-    <h1 style="font-size: x-large" class="headline mb-0">Enter Your Email</h1>
+    <h1 style="font-size: x-large" class="headline mb-0">Enter Your Email To Login</h1>
   </div>
   <v-card-text>
     <v-text-field v-model="email" prepend-icon="email" name="email" label="Email" type="email"></v-text-field>
